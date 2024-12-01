65d70fb4850c3e7da162138eb01b95a25ceb6a04
CsonkZolt <100192820+CsonkZolt@users.noreply.github.com>
2024-12-01 03:23:51 +0100

    mn

--- BlazorApp6/obj/Debug/net8.0/BlazorApp6.AssemblyInfo.cs
+++ BlazorApp6/obj/Debug/net8.0/BlazorApp6.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e102cd4b5dc266a5e98e8e7c2fde5c8a8892a7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bea1a8e9095030a7d832020ac693ce5ad315456")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

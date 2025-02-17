b42698ff12a7ef4e388ce0d445865040280e0df7
Rossi24211 <olahmarkinfo21@gmail.com>
2025-02-17 10:13:06 +0100

    nagyon joooooooooooo

--- Szakdolgozat/Components/Pages/Registration.razor.cs
+++ Szakdolgozat/Components/Pages/Registration.razor.cs
@@ -6,6 +6,11 @@ namespace SzakDolgozat.Components.Pages
     public partial class Registration
     {
         string value;
+
+        private void OnClick(string text)
+        {
+            //NotificationService.Notify(new NotificationMessage { Severity = NotificationSeverity.Info, Summary = "Button Clicked", Detail = text });
+        }
     }
 
 
--- Szakdolgozat/obj/Debug/net8.0/Szakdolgozat.AssemblyInfo.cs
+++ Szakdolgozat/obj/Debug/net8.0/Szakdolgozat.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SzakDolgozat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99f547f7bd6ef08edff230f6e7c52a272f13e718")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1dd7b1fc5637cd3b30f77a8ac2866639ad29268")]
 [assembly: System.Reflection.AssemblyProductAttribute("SzakDolgozat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SzakDolgozat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

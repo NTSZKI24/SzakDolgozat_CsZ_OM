f1aa8588c9c7494db8d095682b6b259c224aefe9
Rossi24211 <olahmarkinfo21@gmail.com>
2025-03-10 08:35:13 +0100

    asd

--- Szakdolgozat/obj/Debug/net8.0/Szakdolgozat.AssemblyInfo.cs
+++ Szakdolgozat/obj/Debug/net8.0/Szakdolgozat.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SzakDolgozat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+032182cab19a3a4ad8d9295fe1a5bd752b80585a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4dea383990ab7f861bc0223baec09c920293b13")]
 [assembly: System.Reflection.AssemblyProductAttribute("SzakDolgozat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SzakDolgozat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
